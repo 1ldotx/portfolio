aa7c70bcc2fd91eae5ae05ddab41f0edefd5d013
Nont-1ldotx <nappdevdotx@gmail.com>
2025-03-21 03:15:25 +0700

    Revert "idk what it is"
    This reverts commit b98257a17629f638fe04358116cf144612d19553.

--- .next/types/app/resume/page.ts
+++ .next/types/app/resume/page.ts
@@ -1,4 +1,4 @@
-// File: D:\Fork\portfolio\portfolio\src\app\resume\page.jsx
+// File: /Users/1l/portfolio/src/app/resume/page.jsx
 import * as entry from '../../../../src/app/resume/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
