b98257a17629f638fe04358116cf144612d19553
Nont Ekongkarn <nappdevdotx@gmail.com>
2025-03-20 19:33:16 +0700

    idk what it is

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/1l/portfolio/src/app/layout.jsx
+// File: D:\Fork\portfolio\portfolio\src\app\layout.jsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
